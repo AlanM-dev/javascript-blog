ba5f01beb1b4e364618021ee3eda7815955b1b6d
Alan Marcinkiewicz Mac <marcinkiewicz.alan@gmail.com>
2020-07-08 14:29:19 +0200

    Ready for loop for-of

---
+++ js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* 
+document.getElementById('test-button').addEventListener('click', function(){
+    const links = document.querySelectorAll('.titles a');
+    console.log('links:', links);
+  });
+*/ 
+
+
+
+
+
+
