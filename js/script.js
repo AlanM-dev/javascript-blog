c5b2310913064969f6083ffba1e2c2da73c84ae2
Alan Marcinkiewicz Mac <marcinkiewicz.alan@gmail.com>
2020-07-08 22:12:31 +0200

    Finish clickHandler - end of module 6.3

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
+{
 'use strict';
 
-
-
-
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked!');
+    console.log('Link was clicked!');    
     //console.log(event);
   
     /* [DONE] remove class 'active' from all article links  */
@@ -15,9 +14,9 @@ const titleClickHandler = function(event){
       activeLink.classList.remove('active');
     }
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
     console.log('clickedElement:', clickedElement);
-    
+
     clickedElement.classList.add('active');
 
     /* [DONE] remove class 'active' from all articles */
@@ -27,11 +26,16 @@ const titleClickHandler = function(event){
       activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href'); 
+    console.log(articleSelector);
+
+    /* [DONE]find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
   
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
@@ -39,4 +43,4 @@ const titleClickHandler = function(event){
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
-
+}
