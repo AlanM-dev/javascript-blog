42ae7112048db2f869eab0f527d5b334d144b5e5
Alan Marcinkiewicz Mac <marcinkiewicz.alan@gmail.com>
2020-07-15 15:25:37 +0200

    Add generateTags function - tags space problem

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
+/* eslint-disable no-inner-declarations */
 {
   'use strict';
 
-  // CLICK HANDLER // -------------------------
+  // MODUŁ 6.3 //
+  // CLICK HANDLER // ---------------------------------------------
   const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked!');    
+    console.log('Link was clicked!');
     //console.log(event);
-  
+
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -28,24 +30,23 @@
     }
 
     /* [DONE] get 'href' attribute from the clicked link */
-    const articleSelector = clickedElement.getAttribute('href'); 
+    const articleSelector = clickedElement.getAttribute('href');
     console.log(articleSelector);
 
     /* [DONE]find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
     console.log(targetArticle);
-  
+
     /* [DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active');
-  };  
+  };
 
-
-  // ARTICLE SELECTOR // -------------------------
+  // MODUŁ 6.4 //
+  // ARTICLE SELECTOR // --------------------------------------------------
   const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles';
 
-  // eslint-disable-next-line no-inner-declarations
   function generateTitleLinks(){
 
     /* [DONE] remove contents of titleList */
@@ -59,7 +60,7 @@
 
     for(let article of articles){
       console.log(article);
-      
+
       /* [DZIALA] get the article id */
       const articleId = article.getAttribute('id');
       console.log(articleId);
@@ -84,7 +85,7 @@
 
     const links = document.querySelectorAll('.titles a');
     console.log(links);
-  
+
     for(let link of links){
       link.addEventListener('click', titleClickHandler);
     }
@@ -93,5 +94,57 @@
 
   generateTitleLinks();
 
-}
+  // MODUŁ 7.2 //
+  // GENERATE TAGS // ----------------------------------------------
+  const optArticleTagsSelector = '.post-tags .list';
 
+  function generateTags(){
+    /* [DONE] find all articles */
+    const articles = document.querySelectorAll(optArticleSelector);
+    console.log(articles);
+
+    /* [DONE] START LOOP: for every article: */
+    for(let article of articles){
+
+      /* [DONE] find tags wrapper */
+      const tagsList = article.querySelector(optArticleTagsSelector);
+      //tagsList.innerHTML = '';
+      console.log(tagsList);
+
+      /* [DONE] make html variable with empty string */
+      let html = '';
+      console.log(html);
+
+      /* [DONE] get tags from data-tags attribute */
+      const articleTags = article.getAttribute('data-tags');
+      console.log(articleTags);
+
+      /* [DONE] split tags into array */
+      const articleTagsArray = articleTags.split(' ');
+      console.log(articleTagsArray);
+
+      /* [DONE] START LOOP: for each tag */
+      for(let tag of articleTagsArray){
+        console.log(tag);
+
+        /* [DONE] generate HTML of the link */
+        const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
+        console.log(linkHTML);
+
+
+        /* [DONE] add generated code to html variable */
+        html = html + linkHTML;
+
+      /* [DONE] END LOOP: for each tag */
+      }
+
+      /* [PROBLEM] insert HTML of all the links into the tags wrapper */
+      tagsList.innerHTML = html;
+
+    /* END LOOP: for every article: */
+    }
+  }
+
+  generateTags();
+
+}
